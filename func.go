8967a484fecadede7cb1e832774a427aefc464b8
Iury Braun <32597667+iurybraun@users.noreply.github.com>
2026-03-08 08:58:20 -0300

    Update func.go

--- func.go
+++ func.go
@@ -1,62 +1,25 @@
 /*
- * Copyright © 2016-2022 Iury Braun
- * Copyright © 2017-2022 Neo7i
+ * Copyright © 2016-2026 Iury Braun
+ * Copyright © 2017-2026 VOLKER
  */
 
 package go_mongodb_dao
 
 import (
-    //"log"
-    "regexp"
-    
-    //"go.mongodb.org/mongo-driver/bson"
+	"regexp"
+
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-func IsValidObjectId(ObjectId string) bool {
-    re := regexp.MustCompile("^[0-9a-fA-F]{24}$")
-    if ok := re.FindString(ObjectId); ok != "" {
-        return true
-    }
-    
-    return false
+var objectIDRegex = regexp.MustCompile("^[0-9a-fA-F]{24}$")
+
+// IsValidObjectId reports whether the given string is a valid 24-character
+// hexadecimal MongoDB ObjectID.
+func IsValidObjectId(objectID string) bool {
+	return objectIDRegex.MatchString(objectID)
 }
 
+// NewObjectID generates a new unique MongoDB ObjectID.
 func NewObjectID() primitive.ObjectID {
-    return primitive.NewObjectID()
+	return primitive.NewObjectID()
 }
-
-
-
-/* INDEX */
-/*err_index := mgodao.CreateIndex(req, "properties", []string{"$text:location.neighborhood", "$text:location.city"})
-if err_index != nil {
-    log.Println(err_index)
-}*/
-/***
-func (m *Dao) CreateIndex(IndexString []string) error {
-    index := mgo.Index{
-        Key: IndexString,
-    }
-    
-    if err:= db.C(m.Collection).EnsureIndex(index); err != nil{
-        if err.Error() != "not found" {
-            log.Println(err)
-        }
-		return err
-	}
-    
-	return nil
-}
-**/
-/*
-func GetIndex(req *http.Request, Collection string, id interface{}) error {
-    return collection(req, Collection).Remove(bson.M{"_id": id})
-    
-    count, err := collection(req, Collection).Find(&qry).Select(&prj).Count()
-    if err != nil {
-        log.Println(err)
-        return nil, count, err
-    }
-}
-*/
