ce7863418eeb669f8ef96cd480ce263d1926874e
Iury Braun <32597667+iurybraun@users.noreply.github.com>
2026-03-08 08:59:06 -0300

    Update gridfs.go

--- gridfs.go
+++ gridfs.go
@@ -1,167 +1,165 @@
 /*
- * Copyright © 2016-2022 Iury Braun
- * Copyright © 2017-2022 Neo7i
- * 
- * Alt:  Id bson.ObjectId  ==>  id interface{}
- * 
- * https://github.com/ReLaboratory/mulbitchorong-backend/blob/ec0bf74c43db49af18540558ec2033a7520b02d9/handle/upload.go
- * https://github.com/c-jimin/codetech/blob/635798468c09fecb3baa86ba037d8a537c0ad2a7/DaoAndModel/file.go
+ * Copyright © 2016-2026 Iury Braun
+ * Copyright © 2017-2026 VOLKER
+ *
+ * References:
+ *  https://github.com/ReLaboratory/mulbitchorong-backend/blob/ec0bf74c43db49af18540558ec2033a7520b02d9/handle/upload.go
+ *  https://github.com/c-jimin/codetech/blob/635798468c09fecb3baa86ba037d8a537c0ad2a7/DaoAndModel/file.go
  */
 
 package go_mongodb_dao
 
 import (
-    "log"
-    //"time"
-    "io"
-    "bufio"
+	"bufio"
 	"bytes"
-    //"net/http"
 	"context"
+	"fmt"
+	"io"
 	"mime/multipart"
-    //"path/filepath"
-    
-    "github.com/pkg/errors"
-    
-    //"fmt"
-    //"image"
-    //"image/draw"
-    //"strconv"
-    //"image/jpeg"
-    //"image/color"
-    //"encoding/base64"
-    //"os"
-    //"io"
-    //"log"
-    //"bytes"
-    //"net/http"
-    //"github.com/nfnt/resize"
-    
-    "go.mongodb.org/mongo-driver/bson"
-    "go.mongodb.org/mongo-driver/bson/primitive"
-    "go.mongodb.org/mongo-driver/mongo/gridfs"
+
+	"github.com/pkg/errors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/gridfs"
 	"go.mongodb.org/mongo-driver/mongo/options"
-    
+
 	"github.com/iurybraun/go-mongodb-dao/utils/encoder"
 )
 
 const defaultFilesBucket = "fs"
 
-func (m *Dao) CreateGridFSObject(file multipart.File, filename, contentType, path, user string) (interface{}, error) {
-    //var conn db.Connection
-    var collection = client.Database(m.Database)  //.Collection(m.Collection)
-	//bucket, err := GetBucket(fileBucket, &conn)
-    var filesBucket string
-    if m.Collection != "" {
-        filesBucket = m.Collection
-    } else {
-        filesBucket = defaultFilesBucket
-    }
-    bucket, err := gridfs.NewBucket(collection, options.GridFSBucket().SetName(filesBucket))
-	//defer conn.Client.Disconnect(context.Background())
+// filesBucketName returns the GridFS bucket name, defaulting to "fs".
+func (m *Dao) filesBucketName() string {
+	if m.Collection != "" {
+		return m.Collection
+	}
+	return defaultFilesBucket
+}
+
+// newBucket creates a GridFS bucket for the DAO's database and bucket name.
+func (m *Dao) newBucket() (*gridfs.Bucket, error) {
+	db := client.Database(m.Database)
+	bucket, err := gridfs.NewBucket(db, options.GridFSBucket().SetName(m.filesBucketName()))
+	if err != nil {
+		return nil, fmt.Errorf("newBucket: %w", err)
+	}
+	return bucket, nil
+}
+
+// CreateGridFSObject uploads a multipart file to GridFS and returns its
+// ObjectID. contentType and path are stored as metadata on the file entry.
+func (m *Dao) CreateGridFSObject(
+	file multipart.File,
+	filename, contentType, path, user string,
+) (interface{}, error) {
+	bucket, err := m.newBucket()
 	if err != nil {
 		return nil, errors.Wrap(err, "upload fail")
 	}
+
 	md5 := encoder.MD5(file)
-    log.Println(md5)
-    //"md5": md5, "contentType": contentType, 
-    opts := options.GridFSUpload().SetMetadata(bson.M{"path" : path, "__v" : 0, "user" : user, "group" : nil, "permission" : nil})
-    uploadStream, uploadStreamErr := bucket.OpenUploadStream(filename, opts)
-	if uploadStreamErr != nil {
-		log.Println(uploadStreamErr)
-        return nil, uploadStreamErr
+
+	uploadOpts := options.GridFSUpload().SetMetadata(bson.M{
+		"path":       path,
+		"__v":        0,
+		"user":       user,
+		"group":      nil,
+		"permission": nil,
+	})
+
+	uploadStream, err := bucket.OpenUploadStream(filename, uploadOpts)
+	if err != nil {
+		return nil, fmt.Errorf("CreateGridFSObject — OpenUploadStream: %w", err)
 	}
-    file.Seek(0, 0)
-    defer uploadStream.Close()
-    writeErr := WriteToGridFile(file, uploadStream)
-    if writeErr != nil {
-        return nil, writeErr
-    }
-    
-    
-    fileID := uploadStream.FileID
-    
-    /* update md5/contentType */
-    var collectionUpdate = client.Database(m.Database).Collection(filesBucket + ".files")
-    _, errUpdate := collectionUpdate.UpdateOne(
+	defer uploadStream.Close()
+
+	if _, err = file.Seek(0, 0); err != nil {
+		return nil, fmt.Errorf("CreateGridFSObject — file.Seek: %w", err)
+	}
+
+	if err = WriteToGridFile(file, uploadStream); err != nil {
+		return nil, err
+	}
+
+	fileID := uploadStream.FileID
+
+	// Persist md5 and contentType fields that GridFS doesn't set natively.
+	filesCollection := client.
+		Database(m.Database).
+		Collection(m.filesBucketName() + ".files")
+
+	_, err = filesCollection.UpdateOne(
 		context.Background(),
 		bson.M{"_id": fileID},
-		bson.M{
-			"$set": bson.M{"md5" : md5, "contentType" : contentType},
-		},
+		bson.M{"$set": bson.M{"md5": md5, "contentType": contentType}},
 	)
-    if errUpdate != nil {
-		return nil, errUpdate
+	if err != nil {
+		return nil, fmt.Errorf("CreateGridFSObject — update md5/contentType: %w", err)
 	}
-    /* end update */
-    
-    return fileID, nil
+
+	return fileID, nil
 }
 
+// WriteToGridFile copies the contents of file into the GridFS upload stream.
 func WriteToGridFile(file multipart.File, uploadStream *gridfs.UploadStream) error {
+	defer file.Close()
+
 	reader := bufio.NewReader(file)
-	defer func() { file.Close() }()
 	buf := make([]byte, 1024)
+
 	for {
 		n, err := reader.Read(buf)
 		if err != nil && err != io.EOF {
-			return errors.New("Could not read the input file")
+			return errors.New("WriteToGridFile: could not read the input file")
 		}
 		if n == 0 {
 			break
 		}
 		if _, err := uploadStream.Write(buf[:n]); err != nil {
-			return errors.New("Could not write to GridFs")
+			return errors.New("WriteToGridFile: could not write to GridFS")
 		}
 	}
-	uploadStream.Close()
+
 	return nil
 }
 
+// ReadGridFSObject downloads a file from GridFS by its ObjectID hex string
+// and returns the raw bytes in a buffer.
 func (m *Dao) ReadGridFSObject(fileID string) (*bytes.Buffer, error) {
-    //var conn db.Connection
-	var collection = client.Database(m.Database)  //.Collection(m.Collection)
-    //bucket, err := GetBucket(fileBucket, &conn)
-    var filesBucket string
-    if m.Collection != "" {
-        filesBucket = m.Collection
-    } else {
-        filesBucket = defaultFilesBucket
-    }
-    bucket, err := gridfs.NewBucket(collection, options.GridFSBucket().SetName(filesBucket))
-    //defer conn.Client.Disconnect(context.Background())
+	bucket, err := m.newBucket()
 	if err != nil {
-		return nil, errors.Wrap(err, "getFile fail")
+		return nil, errors.Wrap(err, "ReadGridFSObject — bucket init fail")
 	}
-    
+
+	_id, err := primitive.ObjectIDFromHex(fileID)
+	if err != nil {
+		return nil, fmt.Errorf("ReadGridFSObject — invalid ObjectID %q: %w", fileID, err)
+	}
+
 	var buffer bytes.Buffer
-	w := bufio.NewWriter(&buffer)
-	_id, _ := primitive.ObjectIDFromHex(fileID)
-    
-	if _, err = bucket.DownloadToStream(_id, w); err != nil {
-		return nil, errors.Wrap(err, "getFile - bucket.DownloadToStream fail")
+	writer := bufio.NewWriter(&buffer)
+
+	if _, err = bucket.DownloadToStream(_id, writer); err != nil {
+		return nil, errors.Wrap(err, "ReadGridFSObject — DownloadToStream fail")
 	}
-    
+
+	if err = writer.Flush(); err != nil {
+		return nil, fmt.Errorf("ReadGridFSObject — flush: %w", err)
+	}
+
 	return &buffer, nil
 }
 
+// RemoveGridFSObject deletes a file from GridFS by its ObjectID.
 func (m *Dao) RemoveGridFSObject(fileID interface{}) error {
-    var collection = client.Database(m.Database)  //.Collection(m.Collection)
-    var filesBucket string
-    if m.Collection != "" {
-        filesBucket = m.Collection
-    } else {
-        filesBucket = defaultFilesBucket
-    }
-    bucket, err := gridfs.NewBucket(collection, options.GridFSBucket().SetName(filesBucket))
-    //defer conn.Client.Disconnect(context.Background())
+	bucket, err := m.newBucket()
 	if err != nil {
-		return errors.Wrap(err, "getFile fail")
+		return errors.Wrap(err, "RemoveGridFSObject — bucket init fail")
 	}
-    
-    if err = bucket.Delete(fileID); err != nil {
-		return errors.Wrap(err, "getFile - bucket.DownloadToStream fail")
+
+	if err = bucket.Delete(fileID); err != nil {
+		return errors.Wrap(err, "RemoveGridFSObject — Delete fail")
 	}
-    
-    return nil
+
+	return nil
 }
